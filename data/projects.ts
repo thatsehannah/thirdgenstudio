5be642244fb4a90f2c38543bd9484c83ec4fef99
E. Hannah <echannah631@gmail.com>
2026-02-17 14:00:35 -0500

    reorganizing projects

--- data/projects.ts
+++ data/projects.ts
@@ -2,16 +2,6 @@ import { ProjectItem } from "@/types";
 
 export const projects: ProjectItem[] = [
   {
-    title: "Solection",
-    thumbnail: "/images/solection.jpg",
-    thumbnailType: "image",
-    type: "mobile",
-    description:
-      "Helping sneakerheads decide what sneakers to wear! This mobile app lets users manage their collection, plan rotations, track wear history, and receive personalized daily sneaker picks based on local weather.",
-    linkText: "View on GitHub",
-    link: "https://github.com/thatsehannah/solection-app",
-  },
-  {
     title: "The Soulcial Well",
     thumbnail: "/images/thesoulcialwell.png",
     thumbnailType: "image",
@@ -22,16 +12,6 @@ export const projects: ProjectItem[] = [
     link: "https://thesoulcialwell.org",
   },
   {
-    title: "Sha Sha Faces",
-    thumbnail: "/images/shashafaces.png",
-    thumbnailType: "image",
-    type: "web",
-    description:
-      "A full-service website for a Los Angeles makeup artist featuring portfolio showcases, service listings, and client booking. Includes a secure admin dashboard for managing appointments, services, and portfolio content",
-    linkText: "Visit Website",
-    link: "https://shashafaces.com",
-  },
-  {
     title: "Beats By E. Hannah",
     thumbnail: "/videos/beatsbyehannah.mp4",
     thumbnailType: "video",
@@ -40,5 +20,25 @@ export const projects: ProjectItem[] = [
       "What started out as a proof of concept for an aspiring musician turned into a passion project for my love of making music. Features a custom built music player with full audio control and a playlist that includes some of my favorite creations.",
     linkText: "Visit Website",
     link: "https://beatsbyehannah.xyz",
+  },
+  {
+    title: "Solection",
+    thumbnail: "/images/solection.jpg",
+    thumbnailType: "image",
+    type: "mobile",
+    description:
+      "Helping sneakerheads decide what sneakers to wear! This mobile app lets users manage their collection, plan rotations, track wear history, and receive personalized daily sneaker picks based on local weather.",
+    linkText: "View on GitHub",
+    link: "https://github.com/thatsehannah/solection-app",
+  },
+  {
+    title: "Sha Sha Faces",
+    thumbnail: "/images/shashafaces.png",
+    thumbnailType: "image",
+    type: "web",
+    description:
+      "A full-service website for a Los Angeles makeup artist featuring portfolio showcases, service listings, and client booking. Includes a secure admin dashboard for managing appointments, services, and portfolio content",
+    linkText: "Visit Website",
+    link: "https://shashafaces.com",
   },
 ];
